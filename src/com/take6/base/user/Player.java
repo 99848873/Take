66d8cd0c8bce642e1f8781acb35a100ad743911a
99848873 <99848873@qq.com>
2015-03-22 21:15:37 +0800

    更新Player.java

--- src/com/take6/base/user/Player.java
+++ src/com/take6/base/user/Player.java
@@ -4,7 +4,6 @@ package com.take6.base.user;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
